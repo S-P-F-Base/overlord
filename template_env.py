491399f328ad6a7d58d4fef2f2e5dbfa6bd14fe5
themanyfaceddemon <neko-d-05@mail.ru>
2025-12-30 23:12:19 +0300

    fix static

--- template_env.py
+++ template_env.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from fastapi.templating import Jinja2Templates
@@ -8,18 +7,12 @@ STATIC_DIR = BASE_DIR / "static"
 
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-USE_ACCEL = os.getenv("FASTAPISTATIC") != "1"
-
 
 def static_with_version(file: str) -> str:
     static_path = STATIC_DIR / file
     v = int(static_path.stat().st_mtime) if static_path.exists() else 0
 
-    if USE_ACCEL:
-        return f"/overlord/static/{file}?v={v}"
-
-    else:
-        return f"/static/{file}?v={v}"
+    return f"/static/{file}?v={v}"
 
 
 templates.env.filters["ver"] = static_with_version
