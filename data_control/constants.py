9a722d037ff93c63315dcdba8d77acf0b6f14e6d
themanyfaceddemon <gastermini357@gmail.com>
2026-04-29 17:13:38 +0300

    Я написал тут какую-то ебучую хуету

--- data_control/constants.py
+++ data_control/constants.py
@@ -1,32 +1,30 @@
 import json
 from pathlib import Path
 
-OVER_ROOT = Path(__file__).parents[1]
-RUN_DIR = Path("/run/spf")
-
 
 class Constants:
     _data = {}
 
+    OVER_ROOT = Path(__file__).parents[1]
+    RUN_DIR = Path("/run/spf")
     OVER_ENV = OVER_ROOT / ".env"
     OVER_CONSTANTS = OVER_ROOT / "constants.json"
     RUNTIME_ENV_DIR = RUN_DIR / "env"
-    SERVICES_REPO_PATH: Path = OVER_ROOT / "repo"
-    SERVICES_FILE_PATH: Path = Path("/etc/systemd/system")
+    SERVICES_REPO_PATH = OVER_ROOT / "repo"
+    SERVICES_FILE_PATH = Path("/etc/systemd/system")
 
     @classmethod
     def load(cls) -> None:
         if not cls.OVER_CONSTANTS.exists():
-            cls._data.clear()
             return
 
-        try:
-            with open(cls.OVER_CONSTANTS, "r", encoding="utf-8") as file:
-                cls._data = json.load(file)
-
-        except Exception:
-            cls._data.clear()
+        with open(str(cls.OVER_CONSTANTS), "r", encoding="utf-8") as file:
+            cls._data = json.load(file)
 
     @classmethod
-    def get_all_data(cls) -> dict[str, str]:
+    def get_data(cls) -> dict[str, str]:
         return cls._data.copy()
+
+    @classmethod
+    def clear_data(cls) -> None:
+        cls._data.clear()
